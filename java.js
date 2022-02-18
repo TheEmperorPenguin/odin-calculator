d5e5a93ce15c45c55b651bce6f5807cf4b783b59
Gabriel <gabriel.touzalin@gmail.com>
2022-02-18 13:14:17 +0100

    added header footer background, js working except if multiple operators at once need to animate github logo

--- java.js
+++ java.js
@@ -1,32 +1,39 @@
 const numbers = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
 const screen = document.getElementById("screen");
-let actualDisplay = "";
+let display = "";
+let lastDisplay = undefined;
+let lastOperator = undefined;
+let currentoperator = "";
+let firstEntry = true;
+let tempDisplay = false;
+let needReset = false;
 
-const add = function (a, b) {
-  return a + b;
+const clearDisplay = function () {
+  if(needReset == true){
+    resetCalculator();
+    return;
+  }
+  screen.innerText = "";
+  display = ""
 };
 
-const subtract = function (a, b) {
+const add = function (a, b = 0) {
+  return +a + +b;
+};
+
+const subtract = function (a, b = 0) {
   return a - b;
 };
 
-const sum = function (array) {
-  let total = 0;
-  for (let i = 0; i < array.length; i++) {
-    total += array[i];
-  }
-  return total;
-};
-
-const multiply = function (a, b) {
+const multiply = function (a, b = 1) {
   return a * b;
 };
 
-const power = function (a, b) {
-  return a ** b;
-};
-
-const divide = function (a, b) {
+const divide = function (a, b = 1) {
+  if (b == 0){
+    return "ERROR";
+  }
   return a / b;
 };
 
@@ -39,28 +46,83 @@ const operate = function (operator, a, b) {
     case "*":
       return multiply(a, b);
     case "/":
-      return divide(a, b);
+      if(b == 0){
+        return "ERROR";
+      }
+      return Math.round(divide(a, b));
     default:
       return "ERROR";
   }
 };
 
-const createNumber = function (value) {
-  let actualDisplay = screen.innerText;
-  if (actualDisplay.length >= 10) {
+function resetCalculator() {
+  display = "";
+  lastDisplay = undefined;
+  lastOperator = undefined;
+  currentOperator = "";
+  firstEntry = true;
+  screen.innerText = display;
+  needReset = false;
+}
+
+function updateDisplay() {
+  screen.innerText = display;
+}
+
+function modifyCurrentValue(str) {
+  if (display.length > 9) {
+    return
+  } else if (tempDisplay == true){
+    clearDisplay();
+    tempDisplay = false;
+  }
+  display = `${display}${str}`;
+  updateDisplay()
+}
+
+function modifyCurrentOperator(operator) {
+  if (operator === "AC") {
+    resetCalculator();
+    return;
+  } else if (firstEntry == true) {
+    if(operator == "="){
+      return;
+    }
+    lastDisplay = display
+    lastOperator = operator
+    clearDisplay();
+    firstEntry = false;
     return;
   }
-  actualDisplay = `${actualDisplay}${value}`;
-  toDisplay(actualDisplay);
-};
-const toDisplay = function (display) {
+  
+  display = operate(lastOperator, lastDisplay, display);
+  lastDisplay = display;
+  lastOperator = operator;
   screen.innerText = display;
-};
+  tempDisplay = true;
+  if(operator == "="){
+    console.log("patate");
+    needReset = true;
+  }
+
+}
+function keyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) modifyCurrentValue(e.key);
+  if (e.key === '=') modifyCurrentOperator(e.key);
+  if (e.key === 'Escape') resetCalculator();
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/'){
+  modifyCurrentOperator(e.key);
+  }
+}
 
 Array.from(numbers).forEach((number) => {
-  console.log(number);
   number.addEventListener("click", function () {
-    createNumber(number.innerText);
-    console.log(number);
+    modifyCurrentValue(number.innerText);
   });
 });
+Array.from(operators).forEach((operator) => {
+  operator.addEventListener("click", function () {
+    modifyCurrentOperator(operator.innerText);
+  });
+});
+window.addEventListener("keydown", keyboardInput);
